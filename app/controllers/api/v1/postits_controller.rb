b3ce453b39790504206fb24fc21d8490088f7987
rafaelcostta13 <rafael.costta13@outlook.com>
2021-07-08 11:59:54 -0300

    Feature: search, all_postiit, create and destroy

--- app/controllers/api/v1/postits_controller.rb
+++ app/controllers/api/v1/postits_controller.rb
@@ -1,19 +1,43 @@
 class Api::V1::PostitsController < ApplicationController
 
+	# GET /api/v1/postit/search/:id
 	def search
-		
+		@post = Postit.find(params[:id])
+		if @post.present?
+			render json: @post, status: 200
+		else
+			render json: {status: 'Erro', message: 'Não encontrado'}
+		end
 	end
 
+	# GET /api/v1/postit/all_postits
 	def all_postits
 		@posts = Postit.all
 		render json: @posts
 	end
 
+	# POST /api/v1/postit/create
 	def create
-		
+		post = Postit.new(permit_params)
+		if post.save
+			render json: {message: 'Cadastrado com sucesso!', status: 200}, status: 200
+		else
+			render json: {message: 'Erro ao salvar no banco de dados', status: 500}
+		end
 	end
 
-	def detroy
-		
+	# DELETE /api/v1/postit/destroy/:id
+	def destroy
+		post = Postit.find(params[:id])
+		if post.destroy
+			render json: {message: 'Post excluido com sucesso!', status: 200}
+		else
+			render json: {message: 'Erro ao excluir do banco de dados :(', status: 500}
+		end
+	end
+
+private
+	def permit_params
+		params.permit(:title, :description)
 	end
 end
